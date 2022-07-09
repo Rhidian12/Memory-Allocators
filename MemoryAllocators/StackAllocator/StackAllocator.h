8c2cd25aa7a5603761e5490f1fd9d1c381fbba4a
Rhidian De Wit <rhidiandewit@gmail.com>
2022-07-09 17:00:33 +0200

    made constructor

--- MemoryAllocators/StackAllocator/StackAllocator.h
+++ MemoryAllocators/StackAllocator/StackAllocator.h
@@ -1,5 +1,30 @@
 #pragma once
-class StackAllocator
-{
-};
+#include <stdlib.h> /* malloc */
 
+template<typename T, size_t N>
+class StackAllocator final
+{
+public:
+	using value_type = T;
+	using pointer = T*;
+	using const_pointer = const T*;
+	using reference = T&;
+	using const_reference = const T&;
+
+	StackAllocator()
+	{
+		pBegin = malloc(N * sizeof(T));
+		pEnd = pBegin + N;
+		StackPointer = pBegin;
+	}
+
+	pointer allocate(const size_t amount)
+	{
+
+	}
+
+private:
+	pointer pBegin;
+	pointer pEnd;
+	pointer StackPointer;
+};
