ecae5d29fe4ba40e9cc091b6d2a69f1647f9a059
Rhidian De Wit <rhidiandewit@gmail.com>
2022-07-09 17:12:42 +0200

    add tests for stack allocator

--- MemoryAllocators/main.cpp
+++ MemoryAllocators/main.cpp
@@ -1,6 +1,29 @@
 #include <iostream>
 
-int main()
+#define CATCH_CONFIG_MAIN
+#include "catch.hpp"
+
+#include "StackAllocator/StackAllocator.h"
+
+TEST_CASE("Stack Allocator Test")
 {
-    std::cout << "Hello World!\n";
+	StackAllocator<int, 10> alloc{};
+
+	REQUIRE(alloc.capacity() != 0);
+	REQUIRE(alloc.capacity() == 10);
+
+	REQUIRE_NOTHROW(alloc.allocate(10));
+	alloc.deallocate(10);
+
+	REQUIRE_THROWS_AS(alloc.allocate(11), std::bad_alloc);
+
+	int* pArr = alloc.allocate(10);
+	REQUIRE(pArr != nullptr);
+
+	for (int i{}; i < 10; ++i)
+	{
+		REQUIRE(pArr + i != nullptr);
+	}
+
+	alloc.deallocate(10);
 }
