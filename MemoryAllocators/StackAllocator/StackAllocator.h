69ef65cd3940efee7e67977865bfdae7296968c5
Rhidian De Wit <rhidiandewit@gmail.com>
2022-07-09 17:24:24 +0200

    fix error in deallocate()

--- MemoryAllocators/StackAllocator/StackAllocator.h
+++ MemoryAllocators/StackAllocator/StackAllocator.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <stdlib.h> /* malloc */
 #include <memory> /* std::distance() */
+#include <stdexcept> /* std::invalid_argument */
 
 template<typename T, size_t N>
 class StackAllocator final
