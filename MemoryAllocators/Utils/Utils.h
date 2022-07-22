ec4db908cc47f5002e0f932b56beac57c0c59625
Rhidian De Wit <rhidiandewit@gmail.com>
2022-07-22 15:45:39 +0200

    modified AlignForward

--- MemoryAllocators/Utils/Utils.h
+++ MemoryAllocators/Utils/Utils.h
@@ -5,7 +5,15 @@ namespace Utils
 	/* https://stackoverflow.com/questions/600293/how-to-check-if-a-number-is-a-power-of-2 */
 	inline bool IsPowerOfTwo(const size_t val) { return (val & (val - 1)) == 0; }
 
-	/* https://embeddedartistry.com/blog/2017/02/22/generating-aligned-memory/ */
+	/* https://indiegamedev.net/2022/03/27/custom-c20-memory-allocators-for-stl-containers/ */
+	inline size_t AlignForward(const void* const p, const size_t wantedAlignment)
+	{
+		return ((reinterpret_cast<size_t>(p) - 1u + wantedAlignment) & -wantedAlignment) - reinterpret_cast<size_t>(p);
+	}
+
 	template<typename T>
-	size_t AlignForward(const T* p, const size_t wantedAlignment) { return ((p + (wantedAlignment - 1)) & ~(wantedAlignment - 1)); }
+	size_t AlignForward(const void* const p, const size_t wantedAlignment)
+	{
+		return ((reinterpret_cast<size_t>(p) - 1u + wantedAlignment) & -wantedAlignment) - reinterpret_cast<size_t>(p) + alignof(T);
+	}
 }
