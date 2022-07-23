2f55df9b126427ae318ea74da805e6a8dc24ae0e
Rhidian De Wit <rhidiandewit@gmail.com>
2022-07-23 15:07:00 +0200

    Fix AlignForward()

--- MemoryAllocators/Utils/Utils.h
+++ MemoryAllocators/Utils/Utils.h
@@ -5,18 +5,17 @@ namespace Utils
 	/* https://stackoverflow.com/questions/600293/how-to-check-if-a-number-is-a-power-of-2 */
 	inline bool IsPowerOfTwo(const size_t val) { return (val & (val - 1)) == 0; }
 
-#pragma warning( push )
-#pragma warning( disable:4146 ) /* Disable minus operator on unsigned type remains unsigned error */
 	/* https://indiegamedev.net/2022/03/27/custom-c20-memory-allocators-for-stl-containers/ */
-	inline size_t AlignForward(const void* const p, const size_t wantedAlignment)
+	size_t AlignForward(const void* const p, const size_t alignment)
 	{
-		return ((reinterpret_cast<size_t>(p) - 1u + wantedAlignment) & -wantedAlignment) - reinterpret_cast<size_t>(p);
-	}
+		// Example: Suppose p = 12 == 0xC. and alignment = 8 == 0x8
+		// (12 + (8 - 1)) & ~(8 - 1)
+		// 19 & ~7
+		// 0x0013 & ~0x0007
+		// 0x0013 & 0xFFF8
+		// 0x0010
+		// 16
 
-	template<typename T>
-	size_t AlignForward(const void* const p, const size_t wantedAlignment)
-	{
-		return ((reinterpret_cast<size_t>(p) - 1u + wantedAlignment) & -wantedAlignment) - reinterpret_cast<size_t>(p) + alignof(T);
+		return (reinterpret_cast<size_t>(p) + (alignment - 1)) & ~(alignment - 1);
 	}
-#pragma warning( pop )
 }
